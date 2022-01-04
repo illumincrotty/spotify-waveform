f9c86096fc7abc7395441745341cda1e374b596c
Brian <5673408+illumincrotty@users.noreply.github.com>
2022-01-04 16:54:43 -0600

    Extract Modal

--- src/lib/components/overlay/overlayLogin.svelte
+++ src/lib/components/overlay/overlayLogin.svelte
@@ -2,7 +2,7 @@
 	import { onMount, tick } from 'svelte';
 	import SpotifyButton from '$lib/components/button/spotifyButton.svelte';
 
-	import Overlay from './overlay.svelte';
+	import OverlayModal from './overlayModal.svelte';
 
 	let loginButton: SpotifyButton;
 
@@ -24,39 +24,10 @@
 
 <svelte:window on:keydown={onKeyDown} />
 
-<Overlay>
-	<div
-		class="box stack"
-		role="dialog"
-		aria-labelledby="login_title"
-		aria-describedby="login_description"
-		aria-modal="true"
-	>
-		<h2 id="login_title">Unauthorized</h2>
-		<p id="login_description">
-			You need to connect spotify to access this feature
-		</p>
-		<SpotifyButton bind:this={loginButton} />
-	</div>
-</Overlay>
-
-<style lang="postcss">
-	.box {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-
-		width: clamp(16rem, 33%, 50%);
-		padding: 1em;
-
-		background-color: var(--bg);
-		color: var(--text);
-
-		word-break: normal;
-
-		display: flex;
-		flex-direction: column;
-		text-align: center;
-	}
-</style>
+<OverlayModal labelledby="login_title" describedby="login_description">
+	<h2 id="login_title">Unauthorized</h2>
+	<p id="login_description">
+		You need to connect spotify to access this feature
+	</p>
+	<SpotifyButton bind:this={loginButton} />
+</OverlayModal>
---
+++ src/lib/components/overlay/overlayModal.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import Overlay from './overlay.svelte';
+	export let labelledby: string;
+	export let describedby: string;
+
+	const onKeyDown = (ev: KeyboardEvent) => {
+		if (ev.key !== 'Tab') return;
+		ev.preventDefault();
+	};
+</script>
+
+<svelte:window on:keydown={onKeyDown} />
+
+<Overlay>
+	<div
+		class="middle-box stack"
+		role="dialog"
+		aria-labelledby={labelledby}
+		aria-describedby={describedby}
+		aria-modal="true"
+	>
+		<slot />
+	</div>
+</Overlay>
+
+<style lang="postcss">
+	.middle-box {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+
+		width: clamp(16rem, 33%, 50%);
+		padding: 1em;
+
+		background-color: var(--bg);
+		color: var(--text);
+
+		word-break: normal;
+
+		display: flex;
+		flex-direction: column;
+		text-align: center;
+	}
+</style>
