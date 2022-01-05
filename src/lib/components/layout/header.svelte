71635ab31e353978633229fcb7aba9174a1b691a
Brian <5673408+illumincrotty@users.noreply.github.com>
2022-01-05 01:22:09 -0600

    Add Hamburger menu move nav to hamburger menu

--- src/lib/components/layout/header.svelte
+++ src/lib/components/layout/header.svelte
@@ -3,58 +3,120 @@
 	import { page } from '$app/stores';
 	import ButtonMode from '$lib/components/button/buttonLightModeDarkMode.svelte';
 	import ButtonTheme from '$lib/components/button/buttonTheme.svelte';
+	import ButtonMenu from '../button/buttonMenu.svelte';
 	export let links: { href: string; label: string }[] = [];
+	import { onMount } from 'svelte';
+	import { slide } from 'svelte/transition';
+
+	let menuOpen = false;
+	let classes = ['shadow-pop'];
+
+	onMount(() => (menuOpen = false));
 </script>
 
-<header id="page-top" role="banner" class="center">
-	<nav class="switcher">
-		<div style="flex-grow: 3">
-			<a
-				href={`${base}/`}
-				id="link-to-base"
-				style="display: block; max-width: 2em"
-			>
-				<img src={`${base}/favicon.svg`} alt="Logo" />
+<header>
+	<div style="display: flex; gap:var(--gap);">
+		<div id="base-container" style="margin-right: auto;">
+			<a href={`${base}/`} id="link-to-base">
+				<img
+					src={`${base}/favicon.svg`}
+					alt="Logo"
+					width="36"
+					height="36"
+				/>
 			</a>
 		</div>
-		<div style="flex-grow: 40;" />
-		{#if $page.url.pathname !== `/`}
-			<div class="shadow-pop">
-				<a sveltekit:prefetch href={`${base}/`}>Home</a>
-			</div>
-		{/if}
-		{#each links as link}
-			{#if $page.url.pathname !== `/${link.href}`}
-				<div class="shadow-pop">
-					<a sveltekit:prefetch href={`${base}/${link.href}`}
-						>{link.label}</a
-					>
-				</div>
-			{/if}
-		{/each}
-		<div class="shadow-pop">
-			<ButtonMode />
-		</div>
-		<div class="shadow-pop">
-			<ButtonTheme />
-		</div>
-	</nav>
+
+		<ButtonMode {classes} />
+		<ButtonTheme {classes} />
+		<ButtonMenu bind:open={menuOpen} {classes} />
+	</div>
+
+	{#if menuOpen}
+		<nav>
+			<ul class="unlist" transition:slide={{ duration: 500 }}>
+				<h2>Navigation</h2>
+
+				{#if $page.url.pathname !== `/`}
+					<li class="shadow-pop">
+						<a sveltekit:prefetch href={`${base}/`}>Home</a>
+					</li>
+				{/if}
+				{#each links as link}
+					{#if $page.url.pathname !== `/${link.href}`}
+						<li class="shadow-pop">
+							<a sveltekit:prefetch href={`${base}/${link.href}`}
+								>{link.label}</a
+							>
+						</li>
+					{/if}
+				{/each}
+			</ul>
+		</nav>
+	{/if}
 </header>
 
-<style lang="postcss">
-	#link-to-base {
-		background: fixed;
-		&:hover,
-		&:focus-visible {
-			outline: 0.2em solid var(--theme);
-		}
+<!-- 
+<header id="page-top" role="banner" class="center switcher">
+	<div id="base-container">
+		<a href={`${base}/`} id="link-to-base">
+			<img
+				src={`${base}/favicon.svg`}
+				alt="Logo"
+				width="36"
+				height="36"
+			/>
+		</a>
+	</div>
+	<div style="flex-grow: 40;" />
 
-		&:active {
-			outline-color: var(--text);
+	<div class="shadow-pop">
+		<ButtonMode />
+	</div>
+	<div class="shadow-pop">
+		<ButtonTheme />
+	</div>
+	<div class="shadow-pop">
+		<ButtonMenu />
+	</div>
+</header> -->
+<style lang="postcss">
+	header {
+		max-width: var(--measure);
+		width: 100%;
+		margin: var(--gap) auto 0;
+	}
+	#base-container {
+		line-height: 0;
+		a {
+			display: block;
+			background: unset;
+			margin-right: auto;
+			height: 36px;
+			width: 36px;
+
+			&:hover,
+			&:focus-visible {
+				outline: 0.2em solid var(--theme);
+			}
+
+			&:active {
+				outline-color: var(--text);
+			}
+			img {
+				margin: none;
+			}
 		}
 	}
 
-	a {
-		text-decoration: none;
+	nav {
+		background: transparent;
+		ul {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			gap: 1em;
+			font-size: 1.25rem;
+		}
 	}
 </style>
