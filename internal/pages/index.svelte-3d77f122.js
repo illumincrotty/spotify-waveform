885323aea86a79fc9968e2b573c2a93ac9ae5db0
illumincrotty <illumincrotty@users.noreply.github.com>
2022-01-20 19:30:26 +0000

    deploy: dca5d4e55963f8fb72bed13edb5708b5243e520a

---
+++ internal/pages/index.svelte-3d77f122.js
@@ -0,0 +1,23 @@
+import{S as G,i as I,s as L,j as $,v as k,E as M,d as l,l as b,w as v,f as c,x as g,p,n as d,A as y,G as z,e as S,t as w,k as x,c as E,a as A,g as T,b as C,F as B,m as D,o as F}from"../chunks/vendor-d001275b.js";import{P as H,S as N}from"../chunks/page-cb999a0b.js";import{S as R}from"../chunks/spotifyButton-cfe722f8.js";import{t as W}from"../chunks/storeSession-3798d66e.js";import"../chunks/buttonBase-cbe3e4d0.js";import"../chunks/stateGen-632403f6.js";import"../chunks/paths-28a87002.js";import"../chunks/singletons-a42a5e91.js";function P(m){let i,s;return i=new R({}),{c(){k(i.$$.fragment)},l(a){v(i.$$.fragment,a)},m(a,t){g(i,a,t),s=!0},i(a){s||(p(i.$$.fragment,a),s=!0)},o(a){d(i.$$.fragment,a),s=!1},d(a){y(i,a)}}}function J(m){let i,s,a,t,r,u,_,q,f,h;t=new N({});let o=m[0]==="empty"&&P();return{c(){i=S("h2"),s=w("Theme:"),a=$(),k(t.$$.fragment),r=$(),u=S("p"),_=w(`A simple tool to generate artwork based off spotify tracks. Lorem ipsum
+		dolor sit amet consectetur adipisicing elit. Asperiores, non praesentium
+		minus consequatur, commodi iste tempora laborum eos nisi eveniet
+		officiis ab doloribus error in, soluta neque. Totam, nihil enim.
+		Mollitia, nobis quis hic ut eaque fugiat animi repellat saepe officiis,
+		quidem officia repudiandae corporis non numquam tempore. Et quam
+		repudiandae laudantium reiciendis nihil a, numquam inventore error
+		aspernatur recusandae voluptas ratione consequatur dolores ab soluta sit
+		tempore, quasi vel amet maiores labore odio? Itaque delectus sequi
+		aliquam eius quibusdam libero porro molestiae nihil repellendus, est
+		voluptate, officia blanditiis recusandae aliquid eaque fuga ipsa
+		consequatur hic iure numquam sapiente tempore.`),q=$(),o&&o.c(),f=x(),this.h()},l(e){i=E(e,"H2",{});var n=A(i);s=T(n,"Theme:"),n.forEach(l),a=b(e),v(t.$$.fragment,e),r=b(e),u=E(e,"P",{class:!0});var j=A(u);_=T(j,`A simple tool to generate artwork based off spotify tracks. Lorem ipsum
+		dolor sit amet consectetur adipisicing elit. Asperiores, non praesentium
+		minus consequatur, commodi iste tempora laborum eos nisi eveniet
+		officiis ab doloribus error in, soluta neque. Totam, nihil enim.
+		Mollitia, nobis quis hic ut eaque fugiat animi repellat saepe officiis,
+		quidem officia repudiandae corporis non numquam tempore. Et quam
+		repudiandae laudantium reiciendis nihil a, numquam inventore error
+		aspernatur recusandae voluptas ratione consequatur dolores ab soluta sit
+		tempore, quasi vel amet maiores labore odio? Itaque delectus sequi
+		aliquam eius quibusdam libero porro molestiae nihil repellendus, est
+		voluptate, officia blanditiis recusandae aliquid eaque fuga ipsa
+		consequatur hic iure numquam sapiente tempore.`),j.forEach(l),q=b(e),o&&o.l(e),f=x(),this.h()},h(){C(u,"class","centered")},m(e,n){c(e,i,n),B(i,s),c(e,a,n),g(t,e,n),c(e,r,n),c(e,u,n),B(u,_),c(e,q,n),o&&o.m(e,n),c(e,f,n),h=!0},p(e,n){e[0]==="empty"?o?n&1&&p(o,1):(o=P(),o.c(),p(o,1),o.m(f.parentNode,f)):o&&(D(),d(o,1,1,()=>{o=null}),F())},i(e){h||(p(t.$$.fragment,e),p(o),h=!0)},o(e){d(t.$$.fragment,e),d(o),h=!1},d(e){e&&l(i),e&&l(a),y(t,e),e&&l(r),e&&l(u),e&&l(q),o&&o.d(e),e&&l(f)}}}function K(m){let i,s,a;return s=new H({props:{title:"Spotify Data",$$slots:{default:[J]},$$scope:{ctx:m}}}),{c(){i=$(),k(s.$$.fragment),this.h()},l(t){M('[data-svelte="svelte-1fzxoal"]',document.head).forEach(l),i=b(t),v(s.$$.fragment,t),this.h()},h(){document.title="Spotify Waveforms"},m(t,r){c(t,i,r),g(s,t,r),a=!0},p(t,[r]){const u={};r&3&&(u.$$scope={dirty:r,ctx:t}),s.$set(u)},i(t){a||(p(s.$$.fragment,t),a=!0)},o(t){d(s.$$.fragment,t),a=!1},d(t){t&&l(i),y(s,t)}}}function O(m,i,s){let a;return z(m,W,t=>s(0,a=t)),[a]}class ie extends G{constructor(i){super();I(this,i,O,K,L,{})}}export{ie as default};
